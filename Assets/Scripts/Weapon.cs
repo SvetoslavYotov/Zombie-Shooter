1e93725d03311db0bb6f0fa5d75b8798da69cd97
Svetoslav <syotov89@gmail.com>
2021-02-09 16:55:49 +0200

    02. Adding Raycasting to weapons

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float maxRange = 100f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, maxRange);
+        Debug.Log("I hit this thing: " + hit.transform.name);
+    }
+}
